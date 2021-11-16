ae6f083efa0625edc232f841e226dd6162ed6071
Kmedrano101 <kevin.ejem18@gmail.com>
2021-11-16 20:06:01 +0100

    Pose Module Mediapipe

---
+++ test/poseModule.py
@@ -0,0 +1,50 @@
+# Importar modulos
+import cv2
+import mediapipe as mp
+import time
+import math
+
+class poseDetector():
+    def __init__(self):
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose()
+    
+    def findPose(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+        return img
+    
+    def findPosition(self, img, draw=True):
+        self.lmList = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                self.lmList.append([id, lm.x, lm.y])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+        return self.lmList
+    
+def main():
+    cap = cv2.VideoCapture(0)
+    pTime = 0
+    detector = poseDetector()
+    while True:
+        success, img = cap.read()
+        img = detector.findPose(img)
+        lmList = detector.findPosition(img, draw=False)
+        #if len(lmList) != 0:
+        #    print(lmList[14])
+        #    cv2.circle(img, (lmList[14][1], lmList[14][2]), 15, (0, 0, 255), cv2.FILLED)
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+if __name__ == "__main__":
+    main()
